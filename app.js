5a04b41f5d35a964a86b02438c1a7017c3ed77e7
stian <suyanomiplays@gmail.com>
2021-03-16 11:54:26 +0100

    counter
    laat de site de keuze's bij houden en aan het einde door sturen naar de partij selector

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
 let currentSubject = 0;
 
+const jumbotron = document.querySelector(".jumbotron");
 const startButton = document.getElementById("startBtn");
 const titleHeader = document.getElementById("title");
 const statementPara = document.getElementById("statement");
+const opinie = document.getElementById("opinie");
+const optionButtons = document.querySelectorAll(".option");
+const selResult = document.getElementById("parties");
+var userInput = [];
+var statementId = 0;
 
+/* best simpel, dit start de stemwijzer */
 
 function start() {
     jumbotron.hidden = true;
-    questions.hidden = false;
+    statementPara.hidden = false;
+    titleHeader.innerText = subjects[0].title;
+    opinie.innerText = subjects[0].statement;
+    optionButtons.forEach(element => {
+        element.addEventListener("click", function () {
+            keepProgress(element.value);
+        })
+    });
+}
+
+/* 
+Elke keer als er op een bepaalde keuze knop wordt geklikt,
+sla de waarde van die knop op
+en ga naar volgende statement
+*/
+
+function keepProgress(choice) {
+    /*
+    choice opslaan in userInput array
+    en doorgaan naar volgende stmt
+    display actuele stmt ID
+     */
+    if (choice != "terug") {
+        userInput[currentSubject] = choice;
+        currentSubject++;
+    }
+
+    if (currentSubject >= subjects.length) {
+        return showParties();
+    }
+
+    titleHeader.innerText = subjects[currentSubject].title;
+    opinie.innerText = subjects[currentSubject].statement;
+    console.log(userInput);
+}
+
+function showParties() {
+    /*
+    display partij namen
+    "next" knop die geselecteerde partijen mee neemt
+    naar eind resultaat.
+    */
+    statementPara.hidden = true;
+    selResult.hidden = false;
 }
