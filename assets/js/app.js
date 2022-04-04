90c60033cc74c340d3a31b128e4696a397a13eae
stian <99058760@mydavinci.nl>
2022-04-04 13:54:48 +0200

    final changes
    made some final adjustments, shortened the code a bit and changed some text/comments.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 startButton.onclick = questions;
-back.onclick = index;
-
+if(currentSubject == 0){
+	back.onclick = index;
+}
 var currentSubject = 0
 var answers = []
 var partyNames = []
@@ -42,12 +43,15 @@ function index(){
 }
 
 function questions(){
+
+	console.log("currentSubject: " + currentSubject);
 	if (currentSubject == subjects.length ){
 		console.log('questionsResult')
 
 		document.getElementById('contra').style.display = 'none';
 		document.getElementById('none').style.display = 'none';
 		document.getElementById('skip').style.display = 'none';
+		document.getElementById('back').style.display = 'none';
 
 		document.getElementById('importantSubjects').style.display ='inline-block'
 
@@ -56,115 +60,105 @@ function questions(){
 		document.getElementById("pro").innerHTML ='ga verder'
 
 		document.getElementById("blueText").innerHTML ='Zijn er onderwerpen die u extra belangrijk vindt?'
-		document.getElementById("questiongText").innerHTML ='Aangevinkte stellingen tellen extra mee bij het berekenen van het resulaat.'
+		document.getElementById("questiongText").innerHTML ='Aangevinkte stellingen tellen extra mee bij het berekenen van het resultaat.'
 
 		pro.onclick = takePoliticalparty;
 		
             // toont checkboxes met stelling
 
-  			 for(var checkboxNumber = 0; checkboxNumber < subjects.length; checkboxNumber++) {
-				var div = document.createElement('div');
-				checkboxList.appendChild(div);
+  		for(var checkboxNumber = 0; checkboxNumber < subjects.length; checkboxNumber++) {
+			var div = document.createElement('div');
+			checkboxList.appendChild(div);
 
-				div.className = 'checkboxDiv' 
-				var checkbox = document.createElement("input");                
-       			checkbox.className = "w3-check";
-				checkbox.id = 'checkbox' + checkboxNumber;
-				checkbox.type = 'checkbox';
-				checkbox.setAttribute("onclick", "checkboxCheck("+checkboxNumber+")")
-       			div.appendChild(checkbox);
+			div.className = 'checkboxDiv' 
+			var checkbox = document.createElement("input");                
+			checkbox.className = "w3-check";
+			checkbox.id = 'checkbox' + checkboxNumber;
+			checkbox.type = 'checkbox';
+			checkbox.setAttribute("onclick", "checkboxCheck("+checkboxNumber+")")
+			div.appendChild(checkbox);
 
-				var checkboxText = document.createElement("p");                
-				checkboxText.id = "checkboxText" + checkboxNumber;
-       			div.appendChild(checkboxText);
+			var checkboxText = document.createElement("p");                
+			checkboxText.id = "checkboxText" + checkboxNumber;
+			div.appendChild(checkboxText);
 
-				document.getElementById("checkboxText" + checkboxNumber).innerHTML = subjects[checkboxNumber].title
-			}
-
-	}
-			else{
-				document.getElementById("blueText").innerHTML = subjects[currentSubject].title
-				document.getElementById("questiongText").innerHTML = subjects[currentSubject].statement
-
-
-				document.getElementById('startButton').style.display ='none'
-				document.getElementById('Header').style.display ='none'
-				document.getElementById('info').style.display ='none'
-
-
-				document.getElementById('questiongText').style.display ='inline-block'
-				document.getElementById('buttons').style.display ='inline-block'
-				document.getElementById('back').style.display ='inline-block'
-
-				pro.onclick = nextAgree;
-				contra.onclick = nextDisagree;
-				back.onclick = backQuestion;
-				skip.onclick = noAnswer;
-				none.onclick = noAnswer;
-				}
-	}
-
-	function checkboxCheck(checkboxNumber){
-		var checkbox = document.getElementById('checkbox' + checkboxNumber);
-		var checkboxText = document.getElementById('checkboxText' + checkboxNumber).innerText;
-		
-  		if (checkbox.checked == true){
-			  console.log('checkboxTrue');
-
-			  // checks if text == data.js // 
-			  for(c = 0; c < subjects.length; c++) {
-				var currentSubject = subjects[c];
-				var myAnswer = answers[c]
-
-			  	if( checkboxText == currentSubject.title){
-					// checks all pro in data.js //
-					for(d = 0; d < currentSubject.parties.length; d++) {
-						this.currentParty[d].name = currentSubject.parties[d]			
-						
-						// checks if partyName == partyScore //
-						for(e = 0; e < this.partyScore.length; e++){
-							// this.partyScore[0].score++;
-								if(this.currentParty[e].name == this.partyScore[e].name){
-									if(currentParty.position == myAnswer){
-										console.log('extra points to ' + partyScore.name )
-										partyScore.score++;
-									}
-								}	
-						}
-					}
-				}
-			  }
+			document.getElementById("checkboxText" + checkboxNumber).innerHTML = subjects[checkboxNumber].title
 		}
+	}
+	else {
+		document.getElementById("blueText").innerHTML = subjects[currentSubject].title
+		document.getElementById("questiongText").innerHTML = subjects[currentSubject].statement
 
-		if (checkbox.checked == false){
-			console.log('checkboxFalse')
 
-				  // checks if text == data.js // 
-				  for(c = 0; c < subjects.length; c++) {
-					var currentSubject = subjects[c];
-					var myAnswer = answers[c]
+		document.getElementById('startButton').style.display ='none'
+		document.getElementById('Header').style.display ='none'
+		document.getElementById('info').style.display ='none'
+
+
+		document.getElementById('questiongText').style.display ='inline-block'
+		document.getElementById('buttons').style.display ='inline-block'
+		document.getElementById('back').style.display ='inline-block'
+
+		pro.onclick = nextAgree;
+		contra.onclick = nextDisagree;
+		back.onclick = backQuestion;
+		skip.onclick = noAnswer;
+		none.onclick = noAnswer;
+	}
+}
+
+function checkboxCheck(checkboxNumber){
+	var checkbox = document.getElementById('checkbox' + checkboxNumber);
+	var checkboxText = document.getElementById('checkboxText' + checkboxNumber).innerText;
 	
-					  if( checkboxText == currentSubject.title){
-						// checks all pro in data.js //
-						for(d = 0; d < currentSubject.parties.length; d++) {
-							var currentParty = currentSubject.parties[d];			
-							
-							// checks if partyName == partyScore //
-							for(e = 0; e < partyScore.length; e++){
-								var partyScore = partyScore[e];
-								
-									if(currentParty.name == partyScore.name){
-										if(currentParty.position == myAnswer){
-											console.log('remove extra points from ' + partyScore.name )
-											partyScore.score--
-										}
-									}	
+	if (checkbox.checked == true){
+		console.log('checkboxTrue');
+
+		// checks if text == data.js // 
+		for(c = 0; c < subjects.length; c++) {
+		var currentSubject = subjects[c];
+		var myAnswer = answers[c]
+
+		if( checkboxText == currentSubject.title){
+			// checks all pro in data.js //
+			for(d = 0; d < currentSubject.parties.length; d++) {
+				this.currentParty[d].name = currentSubject.parties[d]			
+				
+				// checks if partyName == partyScore //
+				for(e = 0; e < this.partyScore.length; e++){
+					// this.partyScore[0].score++;
+						if(this.currentParty[e].name == this.partyScore[e].name){
+							if(currentParty.position == myAnswer){
+								console.log('extra points to ' + partyScore.name )
+								partyScore.score++;
 							}
-						}
+						}	
+				}
+			}
+		}
+		else if(checkbox.checked == false){
+			if( checkboxText == currentSubject.title){
+				// checks all pro in data.js //
+				for(d = 0; d < currentSubject.parties.length; d++) {
+					var currentParty = currentSubject.parties[d];			
+					
+					// checks if partyName == partyScore //
+					for(e = 0; e < partyScore.length; e++){
+						var partyScore = partyScore[e];
+						
+							if(currentParty.name == partyScore.name){
+								if(currentParty.position == myAnswer){
+									console.log('remove extra points from ' + partyScore.name )
+									partyScore.score--
+								}
+							}	
 					}
-				  }
+				}
+			}
+		}
 		}
 	}
+}
 
 function nextAgree(){
 	console.log('questionsTrue');
@@ -188,9 +182,12 @@ function nextDisagree(){
 
 function backQuestion(){
 	console.log('backQuestion');
-
-	currentSubject--;
-
+	if(currentSubject == 0){
+		index();
+	}
+	else{
+		currentSubject--;
+	}
 	questions();
 }
 
@@ -209,25 +206,13 @@ function takePoliticalparty(){
 		for(a= 0;a<subjects[b].parties.length;a++){
 			var partyPosition = subjects[b].parties[a].position;
 			var partyName = subjects[b].parties[a].name;
-
-			// checks if answers == party position //
+			// checks if your answer is equal to a party their position on the matter//
 			if(answers[b] == partyPosition){
 				console.log("je bent het eens met " + partyName + ", jullie hadden beide " + partyPosition);
 				var foundIndex = partyScore.findIndex((partyScoreObject) => partyScoreObject.name == partyName);
 				// var posIndex = partyScore.findIndex((partyScoreObject) => partyScoreObject.position == partyName);
-				
-				
-				partyScore[foundIndex].score++;
-
-					// checks if partyName == partyscore // 
-					// for(c = 0; c < partyScore.length; c++){
-					// 	if(partyName === partyScore[c].name){
-					// 		Checker = document.getElementById("checkbox" + c);
-					// 		if(currentParty[a].position == answers[a].position && Checker.checked == true){
-					// 			partyScore[posIndex].score++;
-					// 		}
-					// 	}
-					// }
+								
+				partyScore[foundIndex].score++;					
 			}
 			else{
 				console.log("je bent het niet eens met " + partyName + ", die koos " + partyPosition);
@@ -247,13 +232,13 @@ function answerDisplay() {
 	document.getElementById('back').style.display ='none'
 
 	document.getElementById('checkboxList').style.display ='none'
-	document.getElementById("blueText").innerHTML = 'Resultaten'
+	document.getElementById("blueText").innerHTML = 'Uw standpunten komen het meest overeen met: '
 
 	partyScore.sort(function (x, y) {
 		return y.score - x.score;
 	});
 
 	for(var index = 0; index < partyScore.length; index++){
-		document.getElementById("namePrint").innerHTML += partyScore[index]['name'] + ' ' + partyScore[index]['score'] + '<br>' ;
-}
+		document.getElementById("namePrint").innerHTML += partyScore[index]['name'] + ': ' + partyScore[index]['score'] + '<br>' ;
+	}
 }
